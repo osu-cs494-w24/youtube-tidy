6392b433c44efef1f5baebe4e900fa09d53ea647
s-egge <ssteele836@gmail.com>
2024-03-04 12:53:46 -0800

    Added storeSlice

---
+++ src/redux/searchSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+import { YoutubeSearchResponse } from "../assets/interfaces";
+
+interface SearchState {
+  searchResults: YoutubeSearchResponse[];
+}
+
+const initialState: SearchState = {
+  searchResults: [],
+};
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    addSearchResults(state, action: PayloadAction<YoutubeSearchResponse>) {
+      let search = state.searchResults.find(
+        (result) => result.query === action.payload.query
+      );
+
+      //if search already exists, update it
+      if (search) {
+        search = action.payload;
+      } else {
+        state.searchResults.push(action.payload);
+      }
+    },
+  },
+});
+
+export default searchSlice.reducer;
+export const { addSearchResults } = searchSlice.actions;
+export const selectSearch = (state: RootState) => state.search.searchResults;
