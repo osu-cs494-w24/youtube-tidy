3b9b5fb5fb03df72d99a28be10e66ae5f12c858d
Ari Zeto <arizetocs@gmail.com>
2024-03-12 15:13:46 -0700

    Made Login it's own component, rendered within homepage, playlists, and subscriptions. Please run locally and ensure each three pages let you login (refresh per page so you can test logging into each). Note, the subscriptions page does not show anything, and the homepage and playlist page render practically the same thing (for now). On success, it should display a welcome message and your playlists (on homepage and playlists page).

--- src/pages/Playlists.tsx
+++ src/pages/Playlists.tsx
@@ -1,23 +1,16 @@
 import { useAppSelector } from "../redux/hooks";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import AllPlaylists from "../components/AllPlaylists";
 import SinglePlaylist from "../components/SinglePlaylist";
 
+import Login from "../components/Login";
+
 function Playlists() {
-  const navigate = useNavigate();
   const playlists = useAppSelector((state) => state.playlists.playlists);
   const user = useAppSelector((state) => state.user.info);
 
-  // redirect to root page if user is not authenticated
-  useEffect(() => {
-    if (!user?.access_token) {
-      navigate("/");
-    }
-  }, [user, navigate]);
-
   return (
     <>
+      <Login />
       <h1>
         {user?.given_name ? `${user.given_name}'s Playlists` : "Playlists"}
       </h1>
