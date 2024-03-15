ca9af766409d7cd2617a9bcf6d96752756b08ee6
Ari Zeto <arizetocs@gmail.com>
2024-03-15 16:43:23 -0700

    Button toggle for showing playlists, button toggle for showing subscriptions (expecting subscription component).

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from "../redux/hooks";
+import { useState } from "react";
 import AllPlaylists from "../components/AllPlaylists";
 import SinglePlaylist from "../components/SinglePlaylist";
 import styled from "@emotion/styled";
@@ -7,12 +8,66 @@ function Homepage() {
   const user = useAppSelector((state) => state.user.info);
   // const playlists = useAppSelector((state) => state.playlists.playlists);
 
+  const [playlistToggle, setPlaylistToggle] = useState(false);
+  const [subscriptionsToggle, setSubscriptionsToggle] = useState(false);
+  const togglePlaylist = () => {
+    setPlaylistToggle((prevIsOn) => !prevIsOn);
+  };
+  const toggleSubscriptions = () => {
+    setSubscriptionsToggle((prevIsOn) => !prevIsOn);
+  };
+
+  const ContainerButtons = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  `;
+
+  const HomepageButton = styled.button`
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    flex-grow: 1;
+    :hover {
+      box-shadow: 10px 5px 5px rgba(252, 210, 211, 0.5);
+    }
+  `;
+
   return (
     <>
       {/* <h1>Homepage</h1> */}
       {user?.access_token ? (
         <>
-          <AllPlaylists />: (<div>No playlists found</div>)
+          <ContainerButtons>
+            {playlistToggle ? (
+              <HomepageButton onClick={togglePlaylist}>
+                {/* {console.log("Am I on?", playlistToggle)} */}
+                Hide Playlists
+              </HomepageButton>
+            ) : (
+              <HomepageButton onClick={togglePlaylist}>
+                {/* {console.log("Am I on?", playlistToggle)} */}
+                Show Playlists
+              </HomepageButton>
+            )}
+            {subscriptionsToggle ? (
+              <HomepageButton onClick={toggleSubscriptions}>
+                {/* {console.log("Am I on?", subscriptionsToggle)} */}
+                Hide Subscriptions
+              </HomepageButton>
+            ) : (
+              <HomepageButton onClick={toggleSubscriptions}>
+                {/* {console.log("Am I on?", subscriptionsToggle)} */}
+                Show Subscriptions
+              </HomepageButton>
+            )}
+            {/* {console.log("Am I on?", playlistToggle)} */}
+          </ContainerButtons>
+          {playlistToggle ? <AllPlaylists /> : <p>Playlists hidden</p>}
+          {subscriptionsToggle ? (
+            <p> PUT COMPONENT HERE RENDERING SUBSCRIPTIONS</p>
+          ) : (
+            <p> Subscriptions hidden</p>
+          )}
         </>
       ) : (
         <>
