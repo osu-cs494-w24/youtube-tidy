335e4eb652888fdca444050423eaaf41e36b3fd6
Kameron Keller <mrkameronkeller@gmail.com>
2024-03-16 12:16:03 -0700

    Add handlers for selecting and unsubscribing

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { loadSubscriptions } from "../redux/subscriptionsSlice";
+import {
+  loadSubscriptions,
+  removeSubscriptions,
+} from "../redux/subscriptionsSlice";
 import SingleSubscription from "../components/SingleSubscription";
 
 function Subscriptions() {
   const [selectedSubscriptions, setSelectedSubscriptions] = useState<string[]>(
     []
   );
+  const [allSelected, setAllSelected] = useState<bool>(false);
+  const [unsubscribedClicked, setUnsubscribedClicked] = useState<bool>(false);
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.info);
   const { subscriptionList, loading } = useAppSelector(
@@ -18,15 +23,11 @@ function Subscriptions() {
     dispatch(loadSubscriptions("1234"));
   }, []);
 
-  useEffect(() => {
-    console.log("== selected1Subscriptions", selectedSubscriptions);
-  }, [selectedSubscriptions]);
-
   function handleSelect(subscriptionId: string, isSelected: bool) {
-    console.log("== subscriptionId", subscriptionId);
-    console.log("== isSelected", isSelected);
+    // console.log("== subscriptionId", subscriptionId);
+    // console.log("== isSelected", isSelected);
     if (isSelected) {
-      console.log("hereherhehere");
+      // console.log("hereherhehere");
       setSelectedSubscriptions([...selectedSubscriptions, subscriptionId]);
     } else {
       const updatedSelectedSubs = selectedSubscriptions.filter(
@@ -35,6 +36,28 @@ function Subscriptions() {
 
       setSelectedSubscriptions(updatedSelectedSubs);
     }
+    setUnsubscribedClicked(false);
+  }
+
+  function handleSelectAll() {
+    if (!allSelected) {
+      setSelectedSubscriptions(
+        subscriptionList.map((subscription) => subscription.id)
+      );
+    } else {
+      setSelectedSubscriptions([]);
+    }
+    setAllSelected(!allSelected);
+    setUnsubscribedClicked(false);
+  }
+
+  function handleUnsubscribe() {
+    // call youtube api to unsubscribe
+    // delete subscriptions from the store
+    dispatch(removeSubscriptions(selectedSubscriptions));
+    setUnsubscribedClicked(false);
+    setAllSelected(false);
+    setSelectedSubscriptions([]);
   }
 
   return (
@@ -45,9 +68,18 @@ function Subscriptions() {
           : "Subscriptions"}
       </h1>
       <div>
-        <button>Select all</button>
-        <button>Unselect all</button>
-        <button>Unsubscribe</button>
+        <button onClick={handleSelectAll}>
+          {allSelected ? "Unselect" : "Select"} all
+        </button>
+        {unsubscribedClicked && selectedSubscriptions.length > 0 ? (
+          <button onClick={handleUnsubscribe}>
+            Confirm unsubscribe from {selectedSubscriptions.length} channels?
+          </button>
+        ) : (
+          <button onClick={() => setUnsubscribedClicked(true)}>
+            Unsubscribe
+          </button>
+        )}
       </div>
       {loading === "fulfilled"
         ? subscriptionList.map((subscriptionData) => (
@@ -55,6 +87,10 @@ function Subscriptions() {
               key={subscriptionData.id}
               subscriptionData={subscriptionData}
               handleSelect={handleSelect}
+              isSelected={
+                allSelected ||
+                selectedSubscriptions.includes(subscriptionData.id)
+              }
             />
             // <p key={subscriptionData.id}>text</p> // assuming 'id' and 'name' are properties of 'subscription'
           ))
