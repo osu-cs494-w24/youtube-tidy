8fa71ebdc10ff9d70dda45eeb9349e3893400a5b
Kameron Keller <mrkameronkeller@gmail.com>
2024-03-15 23:13:50 -0700

    Stub out initial subscriptions page

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,7 +1,18 @@
-import { useAppSelector } from "../redux/hooks";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { loadSubscriptions } from "../redux/subscriptionsSlice";
+
 
 function Subscriptions() {
+  const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.info);
+  const { subscriptionList, loading } = useAppSelector( ( state )=> state.subscriptions );
+
+  useEffect(() => {
+    // This is a fake access token while using dummy data
+    dispatch(loadSubscriptions("1234"));
+    }, []);
+
 
   return (
     <>
@@ -10,6 +21,12 @@ function Subscriptions() {
           ? `${user.given_name}'s Subscriptions`
           : "Subscriptions"}
       </h1>
+      {console.log("== subscriptionList", subscriptionList)}
+      {loading === "fulfilled"
+      ? subscriptionList.map((subscription) => (
+          <p key={subscription.id}>text</p> // assuming 'id' and 'name' are properties of 'subscription'
+        ))
+      : "loading..."}
     </>
   );
 }
