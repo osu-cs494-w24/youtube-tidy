06ec72ba3cef3705a62f21da4cdd8e7bf69761d2
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2024-04-01 20:17:46 -0700

    Remove expansion of ONID from note

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -109,10 +109,7 @@ function Login() {
               account, and use YouTube Tidy to quickly move videos between
               playlists, mass delete or add videos to playlists, and more!
             </p>
-            <p>
-              Note, you must use your Oregon State Network ID (ONID) account to
-              login.
-            </p>
+            <p>Note: You must use an ONID account to login.</p>
             <ReCAPTCHA
               // sitekey={reCAPTCHA_SITE_KEY}
               sitekey={desiredSiteKey}
